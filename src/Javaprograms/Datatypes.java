f87d7cdf344d61f95e31de3e7ca165ef4c40af9c
bharath <bharath@gmail.com>
2023-09-21 14:26:43 +0000

    Done

---
+++ src/Javaprograms/Datatypes.java
@@ -0,0 +1,16 @@
+package Javaprograms;
+
+public class Datatypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=10;
+		String s1="Basic";
+		double f2= 12.0;
+		System.out.println(i);
+		System.out.println(s1);
+		System.out.println(f2);
+
+	}
+
+}
